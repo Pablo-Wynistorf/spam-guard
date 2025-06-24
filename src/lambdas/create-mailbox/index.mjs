eea68013546155ff53a90faaeb4d38a92e789e37
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2025-06-24 15:36:56 +0200

    updatae some stuff

--- src/lambdas/create-mailbox/index.mjs
+++ src/lambdas/create-mailbox/index.mjs
@@ -8,12 +8,11 @@ const TABLE_NAME = process.env.TABLE_NAME;
 const dynamodb = new DynamoDBClient({});
 
 function generateRandomString() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
     for (let i = 0; i < 8; i++) {
         result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
-    result = result.toLowerCase();
     return result;
 }
 
